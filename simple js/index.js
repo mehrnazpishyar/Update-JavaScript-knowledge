4a10c0aac688cd2398f3ae8ab013641976d86c64
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-02-08 15:10:08 +0330

    inheritance of class

--- simple js/index.js
+++ simple js/index.js
@@ -76,10 +76,55 @@ function startProgram() {
 
 
 function happyBirthday(userName, age) {
-    console.log("happy birthday", userName)
-    console.log("you are", age)  
+    // console.log("happy birthday", userName)
+    // console.log("you are", age)  
 }
 
 //***************************************
 
+//inheritance
 
+class Animal {
+    alive = true;
+    eat(){
+        console.log(`this ${this.name} is eating`)
+    }
+
+    sleep(){
+        console.log(`this ${this.name} is sleeping`)
+    }
+}
+
+class Rabbit extends Animal{
+    name = "rabbit";
+
+    run(){
+        console.log(`this ${this.name} is running`)
+    }
+
+}
+
+
+class Fish extends Animal {
+    name = "fish";
+
+    swim(){
+        console.log(`this ${this.name} is swimming`)
+    }
+
+}
+
+class Hawk extends Animal {
+    name = "hawk";
+
+    fly(){
+        console.log(`this ${this.name} is flying`)
+    }
+
+}
+
+const rabbit = new Rabbit();
+const fish = new Fish();
+const hawk = new Hawk();
+
+console.log(rabbit.alive)
