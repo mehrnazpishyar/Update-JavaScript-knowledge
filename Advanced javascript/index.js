ed18e99e7f9ba72dd0ff37f783e5f04447284ee3
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-02-10 22:53:09 +0330

    curring function

--- Advanced javascript/index.js
+++ Advanced javascript/index.js
@@ -15,17 +15,18 @@
 // outer()
 
 // ****************************************
+//closure
 
-function outer(){
-    let counter = 0
-    function inner(){
-        counter++
-        console.log(counter)
-    }
-    inner()
-}
-outer()
-outer()
+// function outer(){
+//     let counter = 0
+//     function inner(){
+//         counter++
+//         console.log(counter)
+//     }
+//     inner()
+// }
+// outer()
+// outer()
 
 //result 
 // 1 
@@ -33,18 +34,38 @@ outer()
 
 // if we want counter add one should write this :
 
-function outer(){
-    let counter = 0
-    function inner(){
-        counter++
-        console.log(counter)
-    }
-    return inner
-}
-const fn = outer()
-fn()
-fn()
+// function outer(){
+//     let counter = 0
+//     function inner(){
+//         counter++
+//         console.log(counter)
+//     }
+//     return inner
+// }
+// const fn = outer()
+// fn()
+// fn()
 
 //result 
 // 1 
 // 2
+
+//****************************************
+//curing function
+
+function sum(a,b,c) {
+    return a+b+c
+}
+
+function curry(fn){
+    return function(a){
+        return function(b){
+            return function(c){
+                return fn(a,b,c)
+            }
+        }
+    }
+}
+
+const curriedSum = curry(sum)
+console.log(curriedSum(2)(3)(6))
