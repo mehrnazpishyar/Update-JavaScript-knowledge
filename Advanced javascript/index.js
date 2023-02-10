c00948797b07c6b389c3f3579468743e78373c4e
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-02-10 22:28:35 +0330

    test closure

--- Advanced javascript/index.js
+++ Advanced javascript/index.js
@@ -1,15 +1,50 @@
+//nested function
+// let a = 10
 
-let a = 10
+// function outer(){
+//     let b = 20
+
+//     function inner(){
+//         let c = 30
+//         console.log(a , b , c)
+//     }
+
+//     inner()
+// }
+
+// outer()
+
+// ****************************************
 
 function outer(){
-    let b = 20
-
+    let counter = 0
     function inner(){
-        let c = 30
-        console.log(a , b , c)
+        counter++
+        console.log(counter)
     }
-
     inner()
 }
-
 outer()
+outer()
+
+//result 
+// 1 
+// 1
+
+// if we want counter add one should write this :
+
+function outer(){
+    let counter = 0
+    function inner(){
+        counter++
+        console.log(counter)
+    }
+    return inner
+}
+const fn = outer()
+fn()
+fn()
+
+//result 
+// 1 
+// 2
