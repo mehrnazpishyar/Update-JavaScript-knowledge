746451d985d32b24174d54093cf898490ce37dff
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-02-10 21:11:34 +0330

    set and delete and  getCookie

--- simple js/index.js
+++ simple js/index.js
@@ -285,6 +285,48 @@ startFile();
 
 //***********************************************
 
+// cookie
+
+// document.cookie = "firstName =SpongeBob; expires= Sun, 1 January 2030 12:00:00 UTC; path=/" ;
+// document.cookie = "lastName = SquarePabts; expires= Sun, 1 January 2000 12:00:00 UTC; path=/" ;
+// console.log(document.cookie)
+
+// setCookie("email", "Sponge@gmail.com",365)
+// deleteCookie("firstName");
+// deleteCookie("lastName");
+// deleteCookie("email");
+
+setCookie("firstName", "SpongeBob", 365);
+setCookie("lastName", "SquarePants", 365);
+
+console.log(getCookie("firstName"))
+console.log(getCookie("lastName"))
 
 
+// console.log(document.cookie)
 
+function setCookie(name, value, daysTolive){
+    const date = new Date();
+    date.setTime(date.getTime() + (daysTolive * 24 * 60 *60 * 1000));
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = `${name}= ${value}; ${expires}; path=/`
+}
+
+
+function deleteCookie(name){
+    setCookie(name, null, null)
+}
+
+function getCookie(name){
+const cDecoded = decodeURIComponent(document.cookie);
+const cArray = cDecoded.split("; ")
+let result = null;
+// console.log(cArray)
+
+cArray.forEach(element => {
+    if(element.indexOf(name)==0){
+result = element.substring(name.lenght + 1)
+    }
+})
+return result;
+}
