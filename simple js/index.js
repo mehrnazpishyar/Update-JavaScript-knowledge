7477e902062b0cda779964ed9edf3e97bcb06bfa
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-02-19 23:31:21 +0330

    math methods

--- simple js/index.js
+++ simple js/index.js
@@ -1534,3 +1534,135 @@ isNaN(new Date().toString()); // true; the string representation of a Date objec
 
 
 //////////////////////////////////////////////
+
+Math.E
+// Euler's number and the base of natural logarithms; approximately 2.718.
+
+Math.LN2
+// Natural logarithm of 2; approximately 0.693.
+
+Math.LN10
+// Natural logarithm of 10; approximately 2.303.
+
+Math.LOG2E
+// Base-2 logarithm of E; approximately 1.443.
+
+Math.LOG10E
+// Base-10 logarithm of E; approximately 0.434.
+
+Math.PI
+// Ratio of a circle's circumference to its diameter; approximately 3.14159.
+
+Math.SQRT1_2
+// Square root of ½; approximately 0.707.
+
+Math.SQRT2
+// Square root of 2; approximately 1.414.
+
+// Static methods
+Math.abs()
+// Returns the absolute value of x.
+
+Math.acos()
+// Returns the arccosine of x.
+
+Math.acosh()
+// Returns the hyperbolic arccosine of x.
+
+Math.asin()
+// Returns the arcsine of x.
+
+Math.asinh()
+// Returns the hyperbolic arcsine of a number.
+
+Math.atan()
+// Returns the arctangent of x.
+
+Math.atanh()
+// Returns the hyperbolic arctangent of x.
+
+Math.atan2()
+// Returns the arctangent of the quotient of its arguments.
+
+Math.cbrt()
+// Returns the cube root of x.
+
+Math.ceil()
+// Returns the smallest integer greater than or equal to x.
+
+Math.clz32()
+// Returns the number of leading zero bits of the 32-bit integer x.
+
+Math.cos()
+// Returns the cosine of x.
+
+Math.cosh()
+// Returns the hyperbolic cosine of x.
+
+Math.exp()
+// Returns ex, where x is the argument, and e is Euler's number (2.718…, the base of the natural logarithm).
+
+Math.expm1()
+// Returns subtracting 1 from exp(x).
+
+Math.floor()
+// Returns the largest integer less than or equal to x.
+
+Math.fround()
+// Returns the nearest single precision float representation of x.
+
+Math.hypot()
+// Returns the square root of the sum of squares of its arguments.
+
+Math.imul()
+// Returns the result of the 32-bit integer multiplication of x and y.
+
+Math.log()
+// Returns the natural logarithm (㏒e; also, ㏑) of x.
+
+Math.log1p()
+// Returns the natural logarithm (㏒e; also ㏑) of 1 + x for the number x.
+
+Math.log10()
+// Returns the base-10 logarithm of x.
+
+Math.log2()
+// Returns the base-2 logarithm of x.
+
+Math.max()
+// Returns the largest of zero or more numbers.
+
+Math.min()
+// Returns the smallest of zero or more numbers.
+
+Math.pow()
+// Returns base x to the exponent power y (that is, xy).
+
+Math.random()
+// Returns a pseudo-random number between 0 and 1.
+
+Math.round()
+// Returns the value of the number x rounded to the nearest integer.
+
+Math.sign()
+// Returns the sign of the x, indicating whether x is positive, negative, or zero.
+
+Math.sin()
+// Returns the sine of x.
+
+Math.sinh()
+// Returns the hyperbolic sine of x.
+
+Math.sqrt()
+// Returns the positive square root of x.
+
+Math.tan()
+// Returns the tangent of x.
+
+Math.tanh()
+// Returns the hyperbolic tangent of x.
+
+Math.trunc()
+// Returns the integer portion of x, removing any fractional digits.
+
+////////////////////////////////////////////
