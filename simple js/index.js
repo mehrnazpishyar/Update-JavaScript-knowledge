58c46519be798a6a7f91179b580c5a43cc23409d
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-04-12 21:46:36 +0330

    hasOwnProperty method

--- simple js/index.js
+++ simple js/index.js
@@ -1896,3 +1896,12 @@ console.log(b) // 5
 
 ////////////////////////////////////////////////
 
+const object1 = {};
+object1.property1 = 42;
+
+console.log(object1.hasOwnProperty('property1')); //true
+
+console.log(object1.hasOwnProperty('toString')); //false
+
+console.log(object1.hasOwnProperty('hasOwnProperty')); //false
+
